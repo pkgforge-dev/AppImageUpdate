97b23ba046dacc7e0a5db5e70505b51e6e9ac1dd
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-04-06 22:47:44 +0000

    deploy with sharun, use uruntime and add description of the project (#6)

--- ci/build-appimages.sh
+++ ci/build-appimages.sh
@@ -3,10 +3,14 @@
 set -ex
 
 export APPIMAGE_EXTRACT_AND_RUN=1
+export ARCH=${ARCH:-"$(uname -m)"}
+
+LIB4BN="https://raw.githubusercontent.com/VHSgunzo/sharun/refs/heads/main/lib4bin"
+APPIMAGETOOL="https://github.com/AppImage/appimagetool/releases/download/continuous/appimagetool-$ARCH.AppImage"
+UPINFO="gh-releases-zsync|pkgforge-dev|AppImageUpdate|latest|*$ARCH.AppImage.zsync"
 
 # use RAM disk if possible
 TEMP_BASE=/tmp
-
 BUILD_DIR="$(mktemp -d -p "$TEMP_BASE" AppImageUpdate-build-XXXXXX)"
 
 cleanup () {
@@ -22,9 +26,6 @@ REPO_ROOT="$(readlink -f "$(dirname "$(dirname "$0")")")"
 OLD_CWD="$(readlink -f .)"
 
 cd "$BUILD_DIR"
-
-export ARCH=${ARCH:-"$(uname -m)"}
-
 cmake "$REPO_ROOT" \
     -DBUILD_QT_UI=OFF \
     -DCMAKE_INSTALL_PREFIX=/usr \
@@ -51,28 +52,78 @@ if [ "$GITHUB_RUN_NUMBER" != "" ]; then
 	export VERSION="$GITHUB_RUN_NUMBER-$VERSION"
 fi
 
-
 # remove unnecessary files from AppDirs
 rm appimageupdatetool.AppDir/usr/bin/AppImageUpdate || true
 rm appimageupdatetool.AppDir/usr/lib/*/libappimageupdate-qt*.so* || true
 rm -rf appimageupdatetool.AppDir/usr/include || true
 find appimageupdatetool.AppDir -type f -iname '*.a' -delete
 
-
-# get linuxdeploy and its qt plugin
-wget https://github.com/TheAssassin/linuxdeploy/releases/download/continuous/linuxdeploy-"$CMAKE_ARCH".AppImage
-wget https://github.com/darealshinji/linuxdeploy-plugin-checkrt/releases/download/continuous/linuxdeploy-plugin-checkrt.sh
-chmod +x linuxdeploy*.AppImage linuxdeploy-plugin-checkrt.sh
-
-
-find appimageupdatetool.AppDir/
-export UPD_INFO="gh-releases-zsync|pkgforge-dev|AppImageUpdate|latest|*$ARCH.AppImage.zsync"
-export OUTPUT="appimageupdatetool"-"$ARCH".AppImage
+mv ./appimageupdatetool.AppDir ./AppDir
+find ./AppDir
 
 # bundle application
-./linuxdeploy-"$CMAKE_ARCH".AppImage --appdir "appimageupdatetool".AppDir --output appimage \
-	-d "$REPO_ROOT"/resources/"appimageupdatetool".desktop -i "$REPO_ROOT"/resources/appimage.png --plugin checkrt
+wget "$LIB4BN" -O ./AppDir/lib4bin && (
+	cd ./AppDir
+	chmod +x ./lib4bin
+	mv ./usr ./shared
+
+	cp -v "$REPO_ROOT"/resources/appimageupdatetool.desktop ./ 
+	cp -v "$REPO_ROOT"/resources/appimage.png ./
+	ln -s appimage.png ./.DirIcon
+
+	./lib4bin -p -v -k -s ./shared/bin/*
+	./sharun -g
+
+	# We need a newer version of glibc since the current one doesn't support --argv0
+	if [ "$ARCH" = x86_64 ]; then
+		wget http://http.us.debian.org/debian/pool/main/g/glibc/libc6_2.41-6_amd64.deb
+	else
+		wget http://http.us.debian.org/debian/pool/main/g/glibc/libc6_2.41-6_arm64.deb
+	fi
+	ar x *.deb
+	tar fx data.tar.xz
+	cp -vr ./usr/lib/*/* ./shared/lib
+	rm -rf ./usr *.tar.* *.deb
+)
+
+echo '#!/bin/sh
+
+CURRENTDIR="$(dirname "$(readlink -f "$0")")"
+BIN="${ARGV0:-$0}"
+BIN="${BIN#./}"
+
+if [ "$1" = --help ] ||[ -z "$1" ]; then
+	>&2 echo ""
+	>&2 echo "    AppImageUpdate Enhanced Edition 💪"
+	>&2 echo "    appimageupdatetool CLI and validate CLI in a single AppImage"
+	>&2 echo ""
+	>&2 echo "    Usage: "
+	>&2 echo "    ${APPIMAGE:-$0} [options...] [<path to AppImage>]" to use appimageupdatetool
+	>&2 echo "    ${APPIMAGE:-$0} validate [options...] [<path to AppImage>]" to use validate
+	>&2 echo ""
+	>&2 echo "    You can also symlink this AppImage as 'validate' to launch the validate CLI directly"
+	>&2 echo ""
+fi
+
+if [ -f "$CURRENTDIR/bin/$BIN" ]; then
+	exec "$CURRENTDIR/bin/$BIN" "$@"
+elif [ -f "$CURRENTDIR/bin/$1" ]; then
+	BIN="$1"
+	shift
+	exec "$CURRENTDIR/bin/$BIN" "$@"
+else
+	exec "$CURRENTDIR/bin/appimageupdatetool" "$@"
+fi' > ./AppDir/AppRun
+chmod +x ./AppDir/AppRun
+
+# Make appimage with uruntime
+wget "$APPIMAGETOOL" -O ./appimagetool
+chmod +x ./appimagetool
+
+echo "Generating AppImage..."
+./appimagetool --comp zstd -n -u "$UPINFO" \
+	"$PWD"/AppDir "$PWD"/appimageupdatetool+validate-"$ARCH".AppImage
 
 # move AppImage to old cwd
-mv appimageupdatetool*.AppImage* "$OLD_CWD"/
+mv appimageupdatetool+validate*.AppImage* "$OLD_CWD"/
 cd -
